2f7922722442ea9a48574d67ee350b02c9b9e870
fSinchuk <51887269+fSinchuk@users.noreply.github.com>
2019-11-24 20:53:12 +0100

    Factory method pattern

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Developer woodDev = new WoodDeveloper("FGK build");
+                House woodHouse = woodDev.Create();
+
+                Developer stoneDev = new StoneDeveloper("IIE house");
+                House stoneHouse = stoneDev.Create();
+        }
+    }
+
+
+
+    public abstract class House {}
+    public class WoodHouse: House
+    {
+            public WoodHouse()
+            {
+                System.Console.WriteLine("Wood house built");
+            }
+    }
+    public class StoneHouse: House
+    {
+            public StoneHouse()
+            {
+                System.Console.WriteLine( "Panel house built");
+            }
+    }
+
+
+    public abstract class Developer {
+        public string Name;
+        public abstract House Create();
+    }
+    public class WoodDeveloper: Developer 
+    {
+        public WoodDeveloper(string name) {
+            base.Name = name;
+        }
+        public override House Create() => new WoodHouse();
+    }
+    public class StoneDeveloper: Developer {
+        public StoneDeveloper(string name)
+        {
+            base.Name = name;
+        }
+        public override House Create() => new StoneHouse();
+    }
+}
